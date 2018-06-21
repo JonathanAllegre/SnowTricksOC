b734653baba0e97759130ef4193ca93b4cb0c445
jhon <jhon@ubuntuServer.home>
2018-06-21 16:25:44 +0200

    fixture for tricks and user works

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,21 +8,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Trick;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
 
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setUsername('Jonathan');
-        $user->setPlainPassword('simple');
+        $user->setPassword($this->encoder->encodePassword($user, 'simple'));
         $user->setEmail('jonathan.allegre258@orange.fr');
         $user->setActive(1);
         $manager->persist($user);
+
+        $manager->flush();
+
+        $id = $user->getId();
+
+        $trick = new Trick();
+
+        $trick->setName('mute');
+        $trick->setDescription('blabla');
+        $trick->setCreated(new \DateTime());
+        $trick->setUser($user);
+
+        $manager->persist($trick);
 
         $manager->flush();
     }
