bb8e2b5386015a6de0189354ffd60552fcafca4d
jhon <jhon@ubuntuServer.home>
2018-06-23 18:32:17 +0200

    Rename UsrChecker ++ Service

--- src/Service/UserCheckerService.php
+++ src/Service/UserCheckerService.php
@@ -14,7 +14,7 @@ use Symfony\Component\Security\Core\Exception\DisabledException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserChecker implements UserCheckerInterface
+class UserCheckerService implements UserCheckerInterface
 {
 
     /**
