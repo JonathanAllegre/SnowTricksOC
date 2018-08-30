c8f33aaddd37dcc508abd7050c597d4aa5f790f5
jhon <jhon@ubuntuServer.home>
2018-08-30 08:18:08 +0200

    TestInProgress

--- tests/PictureServiceTest.php
+++ tests/PictureServiceTest.php
@@ -8,13 +8,17 @@
 
 namespace App\Tests;
 
+use App\Entity\Trick;
 use App\Service\PictureService;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PictureServiceTest extends KernelTestCase
 {
 
+    private $file;
+    private $image;
     /**
      * TEST GET TARGET DIRECTORY
      * MUST RETURN "public/img.tricks"
@@ -30,7 +34,40 @@ class PictureServiceTest extends KernelTestCase
         $this->assertEquals('public/img/tricks', $pictureService->getTargetDirectory());
     }
 
+    public function testUpload()
+    {
+        $targetDirectory = getenv('TARGET_IMG_TRICKS_DIRECTORY');
+        $objectManager   = $this->getContainer()->get(ObjectManager::class);
 
+        $pictureService = new PictureService($targetDirectory, $objectManager);
+
+        $photoPath    = '/Users/jonathan/Desktop/test.jpg';
+        $photoPngPath = '/Users/jonathan/Desktop/Small-mario.png';
+
+        //$photo[] = new UploadedFile($photoPath, 'photo.jpg', 'image/jpeg', null);
+        $photo = new UploadedFile($this->image, 'Small-mario.png', 'image/png', null, true);
+
+        //dd($this->image);
+
+        $uploadedFile = $this->createMock(UploadedFile::class);
+        $uploadedFile->expects($this->any())->method('move')->willReturn('kjlkj');
+
+
+        $pictureService->upload($photo);
+    }
+
+
+    public function setUp()
+    {
+        $this->file = tempnam(sys_get_temp_dir(), 'upl');
+        imagepng(imagecreatetruecolor(10, 10), $this->file); // create and write image/png to it
+        $this->image = new UploadedFile(
+            $this->file,
+            'new_image.png'
+        );
+
+
+    }
     private function getContainer()
     {
         self::bootKernel();
