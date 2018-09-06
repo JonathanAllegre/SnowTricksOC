d0a961228233fe2ad0aafb092887e7c71bdc441e
jhon <jhon@ubuntuServer.home>
2018-09-06 22:27:46 +0200

    test methodForgotPassWord ok

--- tests/UserServiceTest.php
+++ tests/UserServiceTest.php
@@ -9,6 +9,7 @@
 namespace App\Tests;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Service\MailerService;
 use App\Service\UserService;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -48,7 +49,77 @@ class UserServiceTest extends KernelTestCase
      */
     public function testForgotPassword()
     {
+        $registryInterface   = $this->getContainer()->get(ObjectManager::class);
+        $mailerService       = $this->getContainer()->get(MailerService::class);
+        $userPasswordEncoder = $this->getContainer()->get(UserPasswordEncoderInterface::class);
+        $sessionInterface    = $this->getContainer()->get(SessionInterface::class);
 
+        $userService = new UserService($registryInterface, $mailerService, $userPasswordEncoder, $sessionInterface);
+
+        // TEST IF USER FOUND & MAILER TRUE
+        // MUST RETURN TRUE
+
+        // MOCK REPO & MANAGER
+        $user = (new User())->setEmail('test@test.com');
+        $userRepo = $this->createMock(UserRepository::class);
+        $userRepo->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn($user);
+
+        $objectManager = $this->createMock(ObjectManager::class);
+        $objectManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($userRepo);
+
+        // MOCK MAILER
+        $mailer = $this->createMock(MailerService::class);
+        $mailer->expects($this->any())
+            ->method('sendResetPass')
+            ->willReturn(true);
+
+        $form = ['username' => 'test'];
+        $userService = new UserService($objectManager, $mailer, $userPasswordEncoder, $sessionInterface);
+        $this->assertTrue($userService->forgotPassword($form));
+
+        // TEST IF USER NOT FOUND & MAILER TRUE
+        // MUST RETURN "Aucun utilsateur trouvé"
+
+        // MOCK REPO & MANAGER
+        $userRepo = $this->createMock(UserRepository::class);
+        $userRepo->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn(false);
+
+        $objectManager = $this->createMock(ObjectManager::class);
+        $objectManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($userRepo);
+
+        $userService = new UserService($objectManager, $mailer, $userPasswordEncoder, $sessionInterface);
+        $this->assertEquals("Aucun utilisateur trouvé", $userService->forgotPassword($form));
+
+        // TEST IF USER FOUND & MAILER FALSE
+        // MUST RETURN "Une erreur est survenue lors de l'envoie du mail"
+
+        // MOCK REPO & MANAGER
+        $userRepo = $this->createMock(UserRepository::class);
+        $userRepo->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn($user);
+
+        $objectManager = $this->createMock(ObjectManager::class);
+        $objectManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($userRepo);
+
+        // MOCK MAILER
+        $mailer = $this->createMock(MailerService::class);
+        $mailer->expects($this->any())
+            ->method('sendResetPass')
+            ->willReturn(false);
+
+        $userService = new UserService($objectManager, $mailer, $userPasswordEncoder, $sessionInterface);
+        $this->assertEquals("Une erreur est survenue lors de l'envoie du mail", $userService->forgotPassword($form));
     }
 
     /**
