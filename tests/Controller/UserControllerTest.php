bfe14ec0bcecdfb0b14ba720c709b3ac9ae2f804
jhon <jhon@ubuntuServer.home>
2018-09-20 08:22:00 +0200

    Test UserController Ok

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -8,6 +8,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -65,6 +66,35 @@ class UserControllerTest extends WebTestCase
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 
+    /**
+     * TEST REST PASS
+     * ASSERT 404 WITH INEXISTING TOKEN
+     *
+     * php bin/phpUnit --filter
+     * testResestPassword tests/Controller/UserControllerTest.php
+     */
+    public function testResestPassword()
+    {
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/reset/password/noExistingToken');
+
+        // ASSERT 404 WITH INEXISTING TOKEN
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+
+        // GET TOKEN ADMIN
+        $container = self::$kernel->getContainer();
+        $container = self::$container;
+        $user      = self::$container->get('doctrine')->getRepository(User::class)->findOneBy(['username' => 'admin']);
+
+        // ASSERT 200 WITH EXISTING TOKEN
+        $crawler = $client->request('GET', '/reset/password/'.$user->getToken());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertCount(1, $crawler->filter('h1'));
+        $this->assertCount(3, $crawler->filter('input'));
+        $this->assertEquals(1, $crawler->filter('html:contains("Réinitialiser le Mot de Passe:")')->count());
+    }
+
 
     private function logIn(Client $client)
     {
@@ -84,6 +114,4 @@ class UserControllerTest extends WebTestCase
         $cookie = new Cookie($session->getName(), $session->getId());
         $client->getCookieJar()->set($cookie);
     }
-
-
 }
